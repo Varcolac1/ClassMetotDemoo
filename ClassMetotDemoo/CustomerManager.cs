b23cb213bb4eeccf9e6274bfa0c8ae03563d2fe2
Alper <mhmtalpr54@gmail.com>
2022-08-29 21:11:07 +0300

    Add project files.

---
+++ ClassMetotDemoo/CustomerManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemoo
+{
+    internal class CustomerManager
+    {
+        List<Customer> customers = new List<Customer>();    
+        internal void Add(Customer customer)
+        {
+            Console.WriteLine("Customer has been added: "+ customer.FirstName+" "+customer.LastName);
+            customers.Add(customer);
+        }
+        internal void Delete(Customer customer)
+        {
+            Console.WriteLine("Customer has been deleted: " + customer.FirstName + " " + customer.LastName);
+            customers.Remove(customer);
+        }
+        internal void List()
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.Id+" "+customer.FirstName+" "+ customer.LastName);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemoo/Program.cs
@@ -0,0 +1,19 @@
+﻿using ClassMetotDemoo;
+
+CustomerManager customerManager = new CustomerManager();
+
+Customer customer1 = new Customer();
+customer1.Id = 123;
+customer1.FirstName = "Alper";
+customer1.LastName = "Ekici";
+
+Customer customer2 = new Customer();
+customer2.Id = 456;
+customer2.FirstName = "Cansu";
+customer2.LastName = "Kırlı";
+
+customerManager.Add(customer1);
+customerManager.Add(customer2);
+customerManager.List();
+customerManager.Delete(customer2);
+customerManager.List();
