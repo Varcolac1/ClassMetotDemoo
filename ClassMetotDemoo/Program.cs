31b420716582bb14c6dbab9b49caf44a71949faa
Alper <mhmtalpr54@gmail.com>
2022-08-29 21:13:39 +0300

    .

--- ClassMetotDemoo/Program.cs
+++ ClassMetotDemoo/Program.cs
@@ -5,12 +5,12 @@ CustomerManager customerManager = new CustomerManager();
 Customer customer1 = new Customer();
 customer1.Id = 123;
 customer1.FirstName = "Alper";
-customer1.LastName = "Ekici";
+customer1.LastName = "Harika";
 
 Customer customer2 = new Customer();
 customer2.Id = 456;
 customer2.FirstName = "Cansu";
-customer2.LastName = "Kırlı";
+customer2.LastName = "Ezik";
 
 customerManager.Add(customer1);
 customerManager.Add(customer2);
